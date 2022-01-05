6eb4f5d1ea032784c737d24a242fcd9c1b1f431a
Jhanvi <jhanvi.ug20@nsut.ac.in>
2022-01-06 03:55:36 +0530

    Material ui added

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,4 +1,8 @@
 import React, { useState } from 'react'
+import AddIcon from '@material-ui/icons/Add';
+import Fab from '@material-ui/core/Fab';
+import Zoom from '@material-ui/core/Zoom';
+
 
 function CreateArea (props) {
   const [note, setNote] = useState({
@@ -6,6 +10,7 @@ function CreateArea (props) {
     content: ''
   })
   function handleChange (event) {
+
     const { name, value } = event.target
     setNote(prevNote => {
       return { ...prevNote, [name]: value }
@@ -19,23 +24,31 @@ function CreateArea (props) {
     })
     event.preventDefault()
   }
+  function expand(){
+    setExpanded(true);
+  }
+  const[isExpanded,setExpanded]=useState(false);
   return (
     <div>
       <form className='create-note'>
-        <input
+      {isExpanded?<input
           name='title'
           value={note.title}
           placeholder='Title'
           onChange={handleChange}
-        />
+        />:null}
+        
         <textarea
+        onClick={expand}
           name='content'
           value={note.content}
           placeholder='Take a note...'
-          rows='3'
+          rows={isExpanded?3:1}
           onChange={handleChange}
         />
-        <button onClick={handleClick}>Add</button>
+        <Zoom in={isExpanded}>
+        <Fab onClick={handleClick}><AddIcon/></Fab>
+        </Zoom>
       </form>
     </div>
   )
